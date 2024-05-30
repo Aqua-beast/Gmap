96405a0f9232366de465e3044d1d8148a07734ce
= <=>
2024-05-31 01:36:18 +0530

    css added and changes in ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import "./main.css";
 import "leaflet/dist/leaflet.css";
 import axios from "axios";
+import { IoIosClose } from "react-icons/io";
 
 // leaflet container has two parts js library and css
 import { AppContext } from "./context/context";
@@ -18,14 +19,25 @@ import { Icon, divIcon, point } from "leaflet";
 import { useEffect, useState } from "react";
 import Auth from "./Components/Auth";
 
-const customIcon = new Icon({
-  iconUrl: require("./icons/placeholder.png"),
-  iconSize: [38, 38], // size of the icon
-  iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
-  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
-});
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
+  const customIcon = new Icon({
+    iconUrl: require("./icons/placeholder.png"),
+    iconSize: [38, 38], // size of the icon
+    iconAnchor: [19, 38], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+  });
+
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: "custom-marker-cluster",
+      iconSize: point(33, 33, true),
+    });
+  };
+
   const { state, dispatch, setName, setEmail, setPopup } =
     useContext(AppContext);
   const [pos, setPos] = useState({
@@ -109,13 +121,20 @@ export default function App() {
         zoom={13}
       >
         {/* OPEN STREEN MAPS TILES */}
+                {/* var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+}); */}
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         {state.username.length > 0 ? <MapComponent /> : null}
-        <MarkerClusterGroup>
+        <MarkerClusterGroup chunkedLoading
+          iconCreateFunction={createCustomClusterIcon}>
           {state.popups.map((popup, index) => (
             <Marker
               key={index}
@@ -130,15 +149,17 @@ export default function App() {
             </Marker>
           ))}
         </MarkerClusterGroup>
-        {state.username.length == 0
-        ?<Marker position={[48.4566, 2.3522]} icon={customIcon}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>: null}
+        {state.username.length == 0 ? (
+          <Marker position={[28.598994891328356, 77.20516517210228]} icon={customIcon}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ) : null}
 
         {showForm && (
           <form
+          className="form-place"
             onSubmit={handleSubmit}
             style={{
               position: "absolute",
@@ -149,6 +170,8 @@ export default function App() {
               zIndex: 1000,
             }}
           >
+            <IoIosClose className="close" onClick={()=>{setShowForm(false)}}/>
+            <br />
             <label htmlFor="title">Title:</label>
             <input
               type="text"
@@ -158,7 +181,6 @@ export default function App() {
               }
               required
             />
-            <br />
             <label htmlFor="description">Description:</label>
             <input
               type="text"
@@ -168,7 +190,6 @@ export default function App() {
               }
               required
             />
-            <br />
             <label htmlFor="rating">Rating:</label>
             <input
               type="number"
@@ -178,7 +199,6 @@ export default function App() {
               }
               required
             />
-            <br />
             <button type="submit">Submit</button>
           </form>
         )}
